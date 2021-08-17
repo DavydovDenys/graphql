2dea22253d1bcb8f8eec24551f160b28211a4b5a
Den <denvanrain@gmail.com>
2021-08-17 18:24:34 +0300

    Add services
    GithubService

---
+++ app/services/github_service.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+class GithubService
+  USERS_API = 'https://api.github.com/users/'
+  REPOS     = '/repos'
+  def initialize(login)
+    @login = login
+  end
+
+  def name
+    url                       = URI([USERS_API, @login].join)
+    @name                     = fetch_data(url)['name']
+    git_name_params           = ActionController::Parameters.new({ git_name: { name: @name || 'NA' } })
+    git_name_params.require(:git_name).permit(:name)
+  end
+
+  def repos
+    url   = URI([USERS_API, @login, REPOS].join)
+
+    repos = fetch_data(url).map { |repo| repo['name'] }
+
+    params = []
+    repos.each do |repo|
+      repo_params           = ActionController::Parameters.new({ repo_name: { name: repo } })
+      permitted_repo_params = repo_params.require(:repo_name).permit(:name)
+      params << permitted_repo_params
+    end
+
+    params
+  end
+
+  private
+
+  def fetch_data(url)
+    reponse = Net::HTTP.get(url)
+    JSON.parse(reponse)
+  end
+end
